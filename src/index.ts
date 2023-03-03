f040499e2751a83e849823f068ac660f668493e0
0TickPulse <0tickpulse@gmail.com>
2023-03-04 05:34:41 +0800

    add iterator to exports

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from "./deepEquals/exports.js";
 export * from "./duration/exports.js";
 export * from "./fetchWithZod/exports.js";
 export * from "./functions/exports.js";
+export * from "./iterators/exports.js";
 export * from "./match/exports.js";
 export * from "./resultsAndOptionals/exports.js";
 export * from "./strings/exports.js";
