7ad38e47976aab6e60675c6a95384de92aa12407
0tickpulse <0tickpulse@gmail.com>
2023-03-11 19:06:05 +0800

    some new iterator functions

--- src/iterators/iterators.test.ts
+++ src/iterators/iterators.test.ts
@@ -1,4 +1,4 @@
-import { range } from "./iterators.js";
+import { chunkArray, formatArray, range } from "./iterators.js";
 
 test("range single arity test", () => {
     expect(range(5)).toEqual([0, 1, 2, 3, 4]);
@@ -12,3 +12,18 @@ test("range double arity test", () => {
     expect(range(0, 0)).toEqual([]);
     expect(range(5, -5)).toEqual([5, 4, 3, 2, 1, 0, -1, -2, -3, -4]);
 });
+
+test("array formatting test", () => {
+    const a = range(5);
+    expect(formatArray(a)).toEqual("0, 1, 2, 3, and 4");
+});
+
+test("array chunk test", () => {
+    const a = range(10);
+    expect(chunkArray(a, 3)).toEqual([
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [9],
+    ]);
+})
--- src/iterators/iterators.ts
+++ src/iterators/iterators.ts
@@ -1,3 +1,5 @@
+import { StringBuilder } from "../index.js";
+
 /**
  * Creates an array of numbers (positive and/or negative) progressing from `start` up to, but not including, `end`.
  * If only one argument is provided, it will return an array of numbers from `0` to that number.
@@ -34,7 +36,10 @@ export function range(start: number, end?: number, step?: number) {
  * @returns The mapped object.
  * @category Iterators
  */
-export function mapObject<T extends Record<PropertyKey, unknown>, U>(object: T, predicate: <K extends keyof T>(key: K, value: T[K], index: number) => U) {
+export function mapObject<T extends Record<PropertyKey, unknown>, U>(
+    object: T,
+    predicate: <K extends keyof T>(key: K, value: T[K], index: number) => U,
+) {
     const result: Record<PropertyKey, U> = {};
     for (let i = 0; i < Object.keys(object).length; i++) {
         const key = Object.keys(object)[i];
@@ -51,4 +56,59 @@ export function fromIterator<T>(iterator: Iterator<T>): T[] {
         next = iterator.next();
     }
     return result;
+}
+
+/**
+ * Formats an array into a string using this format: `value1, value2, ..., and value3`.
+ * Values are converted to strings using `String()`.
+ * Follows the Oxford comma.
+ *
+ * @example
+ * ```ts
+ * formatArray([1, 2, 3]); // "1, 2, and 3"
+ * formatArray([1, 2]); // "1 and 2"
+ * formatArray([1]); // "1"
+ * formatArray([]); // ""
+ * ```
+ * @param array The array to format.
+ * @category Iterators
+ */
+export function formatArray<T>(array: T[]): string {
+    let str = "";
+    for (let i = 0; i < array.length; i++) {
+        str += String(array[i]);
+        if (i === array.length - 2) {
+            str += ", and ";
+        } else if (i !== array.length - 1) {
+            str += ", ";
+        }
+    }
+    return str;
+}
+
+/**
+ * Splits an array into chunks of a specified size.
+ * The last chunk may be smaller than the specified size.
+ * If the specified size is less than or equal to `0`, it will return an empty array.
+ *
+ * @param array The array to split.
+ * @param size  The size of each chunk.
+ * @category Iterators
+ */
+export function chunkArray<T>(array: T[], size: number): T[][] {
+    const result: T[][] = [];
+    for (let i = 0; i < array.length; i += size) {
+        result.push(array.slice(i, i + size));
+    }
+    return result;
+}
+
+/**
+ * Converts a value into an array if it isn't already.
+ *
+ * @param value The value to convert.
+ * @category Iterators
+ */
+export function asArray<T>(value: T | T[]): T[] {
+    return Array.isArray(value) ? value : [value];
 }
